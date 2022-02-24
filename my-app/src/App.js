1e4118aad106d1f95415db364962d08b13aa301a
Brunowbbs <engwesleybruno@gmail.com>
2022-02-24 14:56:49 -0300

    image import

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
+import logo from "./download.png"
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
       <p>Este e um paragrafo</p>
+      <img src={logo} alt="logo do react"/>
   </>
   
   )
