f813ec12d07a63f604b8797748ab41361668c2ea
Wesley Bruno <engwesleybruno@gmail.com>
2022-03-18 16:18:19 -0300

    aula checkpoint

--- aula07_parte02/src/App.js
+++ aula07_parte02/src/App.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import "./styles.css";
 
+const task = {
+  id: "1",
+  nome: "Fazer Café",
+  status: false,
+};
+
 function App() {
+  const [textoInput, setTextoInput] = useState("");
+
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      nome: "Fazer Café",
+      status: false,
+    },
+    {
+      id: "2",
+      nome: "Estudar React",
+      status: false,
+    },
+  ]);
+
+  function handleTextInput(event) {
+    setTextoInput(event.target.value);
+  }
+
   return (
     <>
       <header className="header">
-        <input className="input" />
-
+        <input className="input" onChange={(event) => handleTextInput(event)} />
         <div className="container_buttons">
           <Button texto="Adicionar" />
           <Button texto="Limpar" />
