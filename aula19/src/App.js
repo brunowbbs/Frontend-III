86f8271c3a3282a8a11bea6c9cb4ed64d79045c5
Wesley Bruno <engwesleybruno@gmail.com>
2022-04-12 15:40:53 -0300

    react modal

--- aula19/src/App.js
+++ aula19/src/App.js
@@ -1,10 +1,30 @@
 import { useEffect, useState } from "react";
-import { formatMoney, formatDate } from "./hooks/useUtils";
+import Modal from "react-modal";
 
+import { formatMoney, formatDate } from "./hooks/useUtils";
 import api from "./services/api";
+
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    width: 500,
+    height: 400,
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+  overlay: {
+    background: "rgba(0,0,0,0.6)",
+  },
+};
 
 function App() {
   const [accounts, setAccounts] = useState([]);
+  const [visibleModal, setVisibleModal] = useState(false);
 
   async function getAccounts() {
     try {
@@ -19,6 +39,10 @@ function App() {
     getAccounts();
   }, []);
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <>
       <h1>Hello World</h1>
@@ -29,6 +53,25 @@ function App() {
           <p>{formatDate(account.date)}</p>
         </div>
       ))}
+      <button onClick={() => setVisibleModal(true)}>Novo Registro</button>
+      <Modal
+        style={customStyles}
+        isOpen={visibleModal}
+        onRequestClose={() => setVisibleModal(false)}
+      >
+        <h1>Cadastrar nova conta</h1>
+
+        <form onSubmit={handleSubmit}>
+          <input placeholder="Email" />
+          <input placeholder="Senha" />
+          <input placeholder="Telefone" />
+
+          <button type="submit">Salvar</button>
+          <button type="button" onClick={() => setVisibleModal(false)}>
+            Cancelar
+          </button>
+        </form>
+      </Modal>
     </>
   );
 }
